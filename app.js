407139a5d320ce9bb79f5777c0e0fdbbe8516b8d
demilade211 <afolabiogunbanwo@gmail.com>
2021-05-05 20:28:12 +0100

    Added verification and fixed bug

--- app.js
+++ app.js
@@ -9,50 +9,63 @@ let submit = document.querySelector("#sub-btn");
 let delete_btn = document.querySelector(".delete");
 
 const objArr =[];
+let id = 0;
 
 submit.addEventListener("click",setTable);
 table.addEventListener("click",removeTable);
 
 function setTable(event){
     event.preventDefault();
-    createAndPush();
-    addToTable(objArr);
+    validateInput(firstName.value,surname.value,age.value, currentLev.value,favClub.value);
 }
 
 function createAndPush(){
     let obj = {
         fName: firstName.value,
+        id: id,
         sName: surname.value,
         Age: age.value,
         current_level: currentLev.value,
-        favourite_club: favClub.value
+        favourite_club: favClub.value,
+        deleted: false
     }
     let myJson = JSON.stringify(obj)
     objArr.push(myJson);
     return objArr;
 }
 function addToTable(arr){
-    let output = ""
+    let row;
+    let position = "beforeend"
     arr.forEach((values)=>{
         let nValue = JSON.parse(values);
-        let row = `
+        row = `
         <tr class = "table-row">
             <td>${nValue.fName}</td>
             <td>${nValue.sName}</td>
             <td>${nValue.Age}</td>
             <td>${nValue.current_level}</td>
             <td>${nValue.favourite_club}</td>
-            <td><i class="far fa-trash-alt delete"></i></td>
+            <td><i class="far fa-trash-alt delete" id = "${id}"></i></td>
         </tr>
         `
-        output+=row
     })
-   return tableBody.innerHTML = output;
+   return tableBody.insertAdjacentHTML(position,row);
 }
 
 function removeTable(event) {
+    let arr = [];
     if(event.target.classList.contains("delete")){//targets the the element in the table with classname delete
         let btn = event.target;//targets the delete btn
         btn.closest("tr").remove();//finds the closest element to the targeted element and removes it
     }
+    //btn.parentNode.parentNode.parentNode.removeChild(btn.parentNode.parentNode);
+}
+function validateInput(...inputs){
+        if(inputs.includes("")){
+            alert("All inputs must be filled");
+        }else{
+            createAndPush();
+            addToTable(objArr);
+            id++;
+        }
 }
